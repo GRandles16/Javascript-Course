786a7411fed2c655670a59e7b7756fd68fb2e724
GRandles16 <gtarandles@gmail.com>
2024-09-28 04:51:36 +0100

    Basic JavaScript Projects Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+window.alert("Hello Universe!!");
+document.write("Hello Milkyway Galaxy!!!" + "<br>");
+
+var A = "This is a string";
+document.write(A + "<br>");
+
+var B = "Welcome to the Jungle";
+window.alert(B + "<br>");
+
+var C = "\"To be or not to be\" said Hamlet.";
+document.write(C + "<br>");
+
+document.write(A + C + "<br>"); // concatenating variables A and C
+
+var D = B + A;
+document.write(D + "<br>");
+
+var family = "The Simpsons", dad = "Homer", mom = "Marge",
+    sons = "Bart", daughters = "Lisa and Magie";
+document.write(sons + "<br>");
+
+document.write(3 + 3); //Adding 3 + 3 using an expression
+document.write("<br><br><br><br>"); //Making space on the web browser
+
+var Sent1 = "This is the beginning of the string";
+var Sent2 = "and this is the end of the string";
+var Sent3 = Sent1 + Sent2;
